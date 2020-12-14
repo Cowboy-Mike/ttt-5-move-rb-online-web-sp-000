310632887f609064d930283afd39a03221db7572
Mike Nanto <mike@thecowboymechanic.com>
2020-12-14 01:42:50 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input)
   converted_input = user_input.to_i - 1
 end
 
-def move (array, index, character = "X")
+def move(array, index, character = "X")
   array[index] = character
 end
 
